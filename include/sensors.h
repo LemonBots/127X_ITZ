19ae3f7f2defce8d0bf90f5e3c61efa9693f3074
Benji <miller.benjamin.daniel@gmail.com>
2017-06-13 17:39:37 -0700

    Addeds sensors.h

---
+++ include/sensors.h
@@ -0,0 +1,22 @@
+#include "main.h"
+#ifndef SENSORS_H_
+#define SENSORS_H_
+
+//Drive Quadrature Encoders (digital)
+#define leftDriveQuadTop 1
+#define leftDriveQuadBottom 2
+
+#define rightDriveQuadTop 3
+#define rightDriveQuadBottom 4
+
+//Main Lift Potentiometer (analog)
+#define mainLiftPot 1
+
+//Mogo Lift Potentiometer (analog)
+#define mogoLiftPot 2
+
+//Drive Gyroscope
+
+#define gyroscope 3
+
+#endif
