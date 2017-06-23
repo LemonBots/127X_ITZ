c51189776c47a00cbb631f85e40acf2cabc25bc0
Aponthis <miller.benjamin.daniel@gmail.com>
2017-06-23 14:37:08 -0700

    Committing code to show warning message for debugging

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -2,7 +2,15 @@
 #include "motors.h"
 #include "sensors.h"
 
+void liftHeight(void * parameter){
+
+}
+
 void operatorControl() {
+	TaskHandle liftPID = taskCreate(liftHeight,  //"unused variable 'liftPID' [-Wunused-variable]"
+                                  TASK_DEFAULT_STACK_SIZE,
+                                  NULL,
+                                  TASK_PRIORITY_DEFAULT);
 	while (1) {
 		delay(20);
 	}
